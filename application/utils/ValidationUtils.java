c7502f0ef229ceb7f8622df9b20c8629ca3bdb22
Lenty <lenty_sprangeres@hotmail.com>
2021-01-24 14:35:03 +0100

    Refactored check if year is leap year

--- application/utils/ValidationUtils.java
+++ application/utils/ValidationUtils.java
@@ -102,15 +102,8 @@ public class ValidationUtils {
     // validate if given year is a leap year to validate the date
     public static boolean leapYear(int year) {
         if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if (year % 4 == 0) {
-            return true;
-        } else {
-            return false;
+            return year % 400 == 0;
         }
+        return year % 4 == 0;
     }
 }
